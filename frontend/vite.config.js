9fc9a1160f755ccac2acee6c9c7178c4189b444f
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2026-03-03 11:22:04 +0530

    fix the github path issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/shubhanshupandey05/ ',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
